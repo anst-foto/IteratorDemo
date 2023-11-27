5325edd367c37dec79b580eb69b2e360069bb406
Starinin Andrey <starininandrey@gmail.com>
2023-11-27 21:20:27 +0300

    Update

--- IteratorDemo/Program.cs
+++ IteratorDemo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IteratorDemo;
+
+var person = new Person()
+{
+    Id = 0,
+    Name = "Andrey",
+    DateTime = DateTime.Now
+};
+
+/*var properties = new PropertyIterator(person);
+foreach (var prop in properties)
+{
+    Console.WriteLine($"{prop.Name} [{prop.Type.Name}] {prop.Value}");
+}*/
+
+foreach (var prop in person.GetProperties())
+{
+    Console.WriteLine($"{prop.Name} [{prop.Type.Name}] {prop.Value}");
+}
+
+var person2 = new Person.Memento(person).GetState();
+foreach (var prop in person2.GetProperties())
+{
+    Console.WriteLine($"{prop.Name} [{prop.Type.Name}] {prop.Value}");
+}
